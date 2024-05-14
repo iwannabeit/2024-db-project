184c463540401cf154ba8f827d1365e83f98f638
iwannabeit <140404545+iwannabeit@users.noreply.github.com>
2024-05-14 21:57:55 +0900

    first commit

--- php-section/demo.php
+++ php-section/demo.php
@@ -13,8 +13,20 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
   die("연결 실패: " . mysqli_connect_error());
 }
-
 echo "연결 성공!";
+
+$sql = "SELECT id, region FROM jjwifi";
+$result = mysqli_query($conn, $sql);
+
+// if($result){
+//   if(mysqli_num_rows($result) > 0){
+//     while($row = mysqli_fetch_assoc($result)){
+//       echo " ID: " . $row["id"];
+//     }
+//   }
+// }
+
+
 
 
 ?>
