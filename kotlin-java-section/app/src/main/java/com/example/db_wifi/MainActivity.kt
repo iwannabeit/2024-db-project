51040a91461d264f34fd6a8833257b5b2cd16bb5
sunguk-4087 <oggpsu@naver.com>
2024-05-16 17:35:44 +0900

    success main pull

--- kotlin-java-section/app/src/main/java/com/example/db_wifi/MainActivity.kt
+++ kotlin-java-section/app/src/main/java/com/example/db_wifi/MainActivity.kt
@@ -32,27 +32,14 @@ import com.naver.maps.map.util.FusedLocationSource
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-
-
-class MainActivity : AppCompatActivity(), OnMapReadyCallback {
-    private var naverMapInfo: List<NaverMapData>? = null
-    private var naverMapList: NaverMapItem? = null
-import com.naver.maps.map.OnMapReadyCallback
-import com.naver.maps.map.overlay.Marker
 import com.naver.maps.map.util.MarkerIcons
 import android.Manifest
-import android.location.Geocoder
-import android.location.Location
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContentProviderCompat.requireContext
-import com.naver.maps.map.CameraUpdate
-import com.naver.maps.map.util.FusedLocationSource
-import okhttp3.Address
-import okio.IOException
-import java.util.Locale
+
 
 
 class MainActivity : FragmentActivity(), OnMapReadyCallback {
+    private var naverMapInfo: List<NaverMapData>? = null
+    private var naverMapList: NaverMapItem? = null
 
     // FusedLocationProviderClient는 manifest에서 위치권한 얻은 후 사용할 수 있습니다!
     private lateinit var fusedLocationClient: FusedLocationProviderClient
@@ -106,21 +93,57 @@ class MainActivity : FragmentActivity(), OnMapReadyCallback {
 
 
         enableEdgeToEdge()
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+    }
 
-        // MapFragment를 통해 지도 초기화
-        val mapFragment = supportFragmentManager.findFragmentById(R.id.map_fragment) as MapFragment?
-            ?: MapFragment.newInstance().also {
-                supportFragmentManager.beginTransaction().add(R.id.map_fragment, it).commit()
+
+    private fun checkLocationPermission() {
+        when {
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                // 권한이 이미 허용되어 있으면 위치 정보 요청
+                requestLocationUpdates()
             }
-        mapFragment.getMapAsync(this)
+            else -> {
+                // 권한이 없을 경우 권한 요청
+                requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+            }
+        }
+    }
+    @SuppressLint("MissingPermission")
+    private fun requestLocationUpdates() {
+        // 현재 위치 요청
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location ->
+                // 위치가 성공적으로 가져와졌을 때 처리
+                val latitude = location?.latitude ?: 0.0
+                val longitude = location?.longitude ?: 0.0
+                Toast.makeText(
+                    this,
+                    "현재 위치: 위도 $latitude, 경도 $longitude",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+            .addOnFailureListener { e ->
+                // 위치 가져오기 실패시 처리
+                Toast.makeText(this, "위치 정보를 가져오는 데 실패했습니다.", Toast.LENGTH_SHORT).show()
+                Log.e("Location", "Failed to get location: ${e.message}")
+            }
+    }    // 위치 정보 요청
 
+
+    private fun openDrawerWithMarkerInfo(markerInfo: String) {
+        markerInfoText.text = markerInfo // 슬라이딩 드로어에 마커 정보 설정
+        drawerLayout.openDrawer(GravityCompat.START) // 슬라이딩 드로어 열기
     }
         override fun onMapReady(naverMap: NaverMap) {
+            this.naverMap = naverMap // naverMap 변수 초기화
 
             //클라이언트 객체 생성
             val naverMapApiInterface = NaverMapRequest.getClient().create(NaverMapApiInterface::class.java)
@@ -151,94 +174,36 @@ class MainActivity : FragmentActivity(), OnMapReadyCallback {
             })
 
 
-            val marker = Marker()
-            marker.position = LatLng(37.5670135, 126.9783740)
-            marker.map = naverMap
+            naverMap.locationSource = locationSource // 기존에 설정된 위치 소스 초기화
+            naverMap.locationTrackingMode = LocationTrackingMode.Follow // 위치 추적 모드를 Follow로 설정
+            naverMap.uiSettings.isLocationButtonEnabled = true // 현위치 버튼
 
-        }
-}
-
-    }
-
-    // 위치 권한 확인 및 요청
-    private fun checkLocationPermission() {
-        when {
-            ContextCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED -> {
-                // 권한이 이미 허용되어 있으면 위치 정보 요청
-                requestLocationUpdates()
-            }
-            else -> {
-                // 권한이 없을 경우 권한 요청
-                requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
-            }
-        }
-    }
-
-    @SuppressLint("MissingPermission")
-    private fun requestLocationUpdates() {
-        // 현재 위치 요청
-        fusedLocationClient.lastLocation
-            .addOnSuccessListener { location ->
-                // 위치가 성공적으로 가져와졌을 때 처리
-                val latitude = location?.latitude ?: 0.0
-                val longitude = location?.longitude ?: 0.0
-                Toast.makeText(
-                    this,
-                    "현재 위치: 위도 $latitude, 경도 $longitude",
-                    Toast.LENGTH_LONG
-                ).show()
-            }
-            .addOnFailureListener { e ->
-                // 위치 가져오기 실패시 처리
-                Toast.makeText(this, "위치 정보를 가져오는 데 실패했습니다.", Toast.LENGTH_SHORT).show()
-                Log.e("Location", "Failed to get location: ${e.message}")
-            }
-    }    // 위치 정보 요청
-
-
-    private fun openDrawerWithMarkerInfo(markerInfo: String) {
-        markerInfoText.text = markerInfo // 슬라이딩 드로어에 마커 정보 설정
-        drawerLayout.openDrawer(GravityCompat.START) // 슬라이딩 드로어 열기
-    }
-
-    @UiThread
-    override fun onMapReady(naverMap: NaverMap) {
-        this.naverMap = naverMap // naverMap 변수 초기화
-
-        naverMap.locationSource = locationSource // 기존에 설정된 위치 소스 초기화
-        naverMap.locationTrackingMode = LocationTrackingMode.Follow // 위치 추적 모드를 Follow로 설정
-        naverMap.uiSettings.isLocationButtonEnabled = true // 현위치 버튼
-
-        //실내지도 활성화
+            //실내지도 활성화
 //        naverMap.isIndoorEnabled = true
 //        naverMap.uiSettings.isIndoorLevelPickerEnabled = true // 실내지도 층 버튼
 
-        // 마커를 클릭했을 때의 동작 설정
-        val marker = Marker()
-        marker.position = LatLng(37.5670135, 126.9783740)
-        marker.map = naverMap
-        marker.icon = MarkerIcons.BLACK
-        marker.iconTintColor = Color.RED
-        marker.width = Marker.SIZE_AUTO
-        marker.height = Marker.SIZE_AUTO
-        marker.setOnClickListener {
-            val cameraPosition = naverMap.cameraPosition
+            // 마커를 클릭했을 때의 동작 설정
+            val marker = Marker()
+            marker.position = LatLng(37.5670135, 126.9783740)
+            marker.map = naverMap
+            marker.icon = MarkerIcons.BLACK
+            marker.iconTintColor = Color.RED
+            marker.width = Marker.SIZE_AUTO
+            marker.height = Marker.SIZE_AUTO
+            marker.setOnClickListener {
+                val cameraPosition = naverMap.cameraPosition
 
-            val latitudeString = cameraPosition.target.latitude.toString() // 위도를 문자열로 변환
-            val longitudeString = cameraPosition.target.longitude.toString() // 경도를 문자열로 변환
-            val positionString : String = "$latitudeString $longitudeString" // 위도와 경도를 합쳐서 위치를 나타내는 문자열 생성
+                val latitudeString = cameraPosition.target.latitude.toString() // 위도를 문자열로 변환
+                val longitudeString = cameraPosition.target.longitude.toString() // 경도를 문자열로 변환
+                val positionString : String = "$latitudeString $longitudeString" // 위도와 경도를 합쳐서 위치를 나타내는 문자열 생성
 
-            openDrawerWithMarkerInfo(positionString) // 마커에 대한 정보를 슬라이딩 드로어에 표시
-            true
+                openDrawerWithMarkerInfo(positionString) // 마커에 대한 정보를 슬라이딩 드로어에 표시
+                true
+            }
+
         }
-
-
-    }
-
-//    companion object {
+    //    companion object {
 //        private const val LOCATION_PERMISSION_REQUEST_CODE = 100
 //    }
+
 }
