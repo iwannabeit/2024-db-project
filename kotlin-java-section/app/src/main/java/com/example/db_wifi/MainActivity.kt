643b6ec093360a7e30ab76247f24490c6100cd76
jaewoo <hahahoho3797@naver.com>
2024-05-25 13:11:29 +0900

    경로안내

--- kotlin-java-section/app/src/main/java/com/example/db_wifi/MainActivity.kt
+++ kotlin-java-section/app/src/main/java/com/example/db_wifi/MainActivity.kt
@@ -36,7 +36,13 @@ import retrofit2.Response
 import com.naver.maps.map.util.MarkerIcons
 import android.Manifest
 import android.graphics.PointF
+import androidx.core.app.ActivityCompat
+import com.naver.maps.map.CameraAnimation
+import com.naver.maps.map.CameraUpdate
 import com.naver.maps.map.overlay.Align
+import com.naver.maps.map.overlay.PathOverlay
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 //import com.naver.maps.map.CameraUpdate
 class MainActivity : FragmentActivity(), OnMapReadyCallback {
@@ -56,6 +62,9 @@ class MainActivity : FragmentActivity(), OnMapReadyCallback {
     private lateinit var naverMap: NaverMap
     private lateinit var drawerLayout: DrawerLayout
     private lateinit var markerInfoText: TextView
+
+    private var currentLatitude: Double? = null
+    private var currentLongitude: Double? = null
 
     // 위치 권한 요청
     private val requestPermissionLauncher =
@@ -88,8 +97,8 @@ class MainActivity : FragmentActivity(), OnMapReadyCallback {
         val mapFragment = fm.findFragmentById(R.id.map) as MapFragment?
             ?: MapFragment.newInstance().also {
                 fm.beginTransaction().add(R.id.map, it).commit()
-            }
-        mapFragment.getMapAsync(this)
+            } // 지도 나타내기
+        mapFragment.getMapAsync(this) // 지도가 준비되었을 때 호출될 메서드 -> onMapReady 대기
 
         // activity_main.xml의 해당 영역을 조작하거나 업데이트 하기위해 불러온 것
         drawerLayout = findViewById(R.id.main) // 배경 영역 선택
@@ -129,11 +138,11 @@ class MainActivity : FragmentActivity(), OnMapReadyCallback {
         fusedLocationClient.lastLocation
             .addOnSuccessListener { location ->
                 // 위치가 성공적으로 가져와졌을 때 처리
-                val latitude = location?.latitude ?: 0.0
-                val longitude = location?.longitude ?: 0.0
+                currentLatitude = location?.latitude ?: 0.0
+                currentLongitude = location?.longitude ?: 0.0
                 Toast.makeText(
                     this,
-                    "현재 위치: 위도 $latitude, 경도 $longitude",
+                    "현재 위치: 위도 $currentLatitude, 경도 $currentLongitude",
                     Toast.LENGTH_LONG
                 ).show()
             }.addOnFailureListener { e ->
@@ -148,35 +157,117 @@ class MainActivity : FragmentActivity(), OnMapReadyCallback {
         markerInfoText.text = markerInfo // 슬라이딩 드로어에 마커 정보 설정
         drawerLayout.openDrawer(GravityCompat.START) // 슬라이딩 드로어 열기
     }
+
+
     override fun onMapReady(naverMap: NaverMap) {
         this.naverMap = naverMap // naverMap 변수 초기화
+        naverMap.isIndoorEnabled = true // 실내지도
 
-        ///////////////////선빈 작업 구간////////////////////////////////////////////////////////
-        //전주 시청으로 카메라 시점 시작
-//            val cameraUpdate = CameraUpdate.scrollTo(LatLng(35.8247083, 127.147528))
-//            naverMap.moveCamera(cameraUpdate)
+        naverMap.locationSource = locationSource // 기존에 설정된 위치 소스 초기화
+        naverMap.locationTrackingMode = LocationTrackingMode.Follow // 위치 추적 모드를 Follow로 설정
+        naverMap.uiSettings.isLocationButtonEnabled = true // 현위치 버튼
+
+
+        val APIKEY_ID = "tgoutvp62u"
+        val APIKEY = "sVfCuiLh1aK2gLTNqEPPn24P5r7gybDHLVEyVibx"
+        //레트로핏 객체 생성
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://naveropenapi.apigw.ntruss.com/map-direction/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val api = retrofit.create(NaverAPI::class.java)
+
+        //근처에서 길찾기
+        val callgetPath = api.getPath(APIKEY_ID, APIKEY, "127.13602285714192 , 35.840335812433025", "127.12944193975801, 35.84678030608311")
+
+        callgetPath.enqueue(object : Callback<ResultPath> { // 비동기 방식으로 API 요청
+            override fun onResponse(call: Call<ResultPath>, response: Response<ResultPath>) { // Response 객체를 통해 응답 데이터를 접근가능
+                val path_cords_list = response.body()?.route?.traoptimal // traoptimal = 실시간 최적경로 넣기
+
+                val path = PathOverlay() // 경로선을 나타내는 위한 오버레이
+
+                // 경로 넣어놓기 위한 공간, MutableList에 add 기능 쓰기 위해 더미 원소(0.1, 0.1) 하나 넣어둠
+                val path_container: MutableList<LatLng> = mutableListOf(LatLng(0.1, 0.1))
+
+                //경로 그리기 응답바디가 List<List<Double>> 이라서 2중 for문 썼음
+                //구한 경로를 하나씩 path_container에 추가
+                path_cords_list?.forEach { path_cords ->
+                    path_cords.path.forEach { path_cords_xy ->
+                        path_container.add(LatLng(path_cords_xy[1], path_cords_xy[0]))
+                    }
+                }
+
+                //더미원소(0.1,0.1) 드랍후 path.coords에 path들을 넣어줌.
+                path.coords = path_container.drop(1)
+                path.color = Color.GREEN
+                path.map = naverMap // 경로선 그리기
+
+                //경로 시작점으로 화면 이동
+                if (path.coords.isNotEmpty()) {
+                    val cameraUpdate = CameraUpdate.scrollTo(path.coords[0])
+                        .animate(CameraAnimation.Fly, 3000)
+                    naverMap.moveCamera(cameraUpdate)
+
+                    Toast.makeText(this@MainActivity, "경로 안내가 시작됩니다.", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onFailure(call: Call<ResultPath>, t: Throwable) {
+                Toast.makeText(this@MainActivity, "경로 안내 실패: ${t.message}", Toast.LENGTH_SHORT).show()
+            }
+        })
 
         // 마커 띄우는 곳!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         val s_marker = Marker()
 
-        val markerList = mutableListOf<Marker>() // 마커 리스트 생성
+        //클라이언트 객체 생성
+        val naverMapApiInterface = NaverMapRequest.getClient().create(NaverMapApiInterface::class.java)
+
+        //응답 받을 콜백 구현
+        val call: Call<NaverMapItem> = naverMapApiInterface.getMapData()
+        Log.v("Debug중","디버그중입니다.")
+
+        //클라이언트 객체가 제공하는 enqueue로 통신에 대한 요청, 응답 처리 방법 명시
+        call.enqueue(object : Callback<NaverMapItem> {
+            override fun onResponse(call: Call<NaverMapItem>, response: Response<NaverMapItem>) {
+                if(response.isSuccessful){
+                    Log.v("디버깅중", "성공!!!!!")
+                    naverMapList = response.body()
+                    naverMapInfo = naverMapList?.jjwifi
+
+                    naverMapInfo?.let{
+                        for(i in 0 until it.size){
+                            val lat = it.get(i).y
+                            val lnt = it.get(i).x
+                            s_marker.position = LatLng(lat, lnt)
+
+                            //클러스터링
+                            clusterer.add(ItemKey(i, LatLng(it.get(i).y, it.get(i).x)), null)
+
+                        }
+                    }
+                    clusterer.map = naverMap
+                }
+
+            }
+            override fun onFailure(call: Call<NaverMapItem>, t: Throwable) {
+                // 통신 실패 시 처리할 코드
+                Log.v("디버깅중", "실패!!!!!")
+            }
+        })
+//        val markerList = mutableListOf<Marker>() // 마커 리스트 생성
 //            // 마커 생성
 //            val marker1 = Marker()
-//            marker1.position = LatLng(35.84033355929488, 127.13602285329539)
-//            val marker2 = Marker()
-//            marker2.position = LatLng(35.83918138324727,  127.12645770115124) // 새로운 위치에 대한 좌표 설정
-//            val marker3 = Marker()
-//            marker3.position = LatLng(35.845793223479376, 127.12963405065182)
-//            // 마커 리스트에 추가
-//            markerList.add(marker1)
-//            markerList.add(marker2)
-//            markerList.add(marker3)
+//            val currentLocation = LatLng(currentLatitude!!,currentLongitude!!)
+//            marker1.position = currentLocation
+//            markerList.add(marker1) // 마커 리스트에 추가
 //
 //            for(marker in markerList){
 //                marker.map = naverMap
 //                marker.icon = MarkerIcons.BLACK
 //                marker.iconTintColor = Color.RED // 마커 겉의 색깔
-//                marker.captionText = "${marker.position.latitude}" // 마크에 글자 설정
+//                marker.captionText = "현재 위치 : ${marker.position.latitude},${marker.position.longitude}" // 마크에 글자 설정
 //                marker.setCaptionAligns(Align.Top) // 글자를 위로
 //                marker.captionTextSize = 15.04f // 텍스트 사이즈
 //                marker.captionOffset = 30 // 마크과 글자 사이 간격
@@ -187,14 +278,7 @@ class MainActivity : FragmentActivity(), OnMapReadyCallback {
 ////                marker.minZoom = 12.0
 ////                marker.maxZoom = 16.0
 //
-////                marker.isHideCollidedMarkers = true // 마커 겹쳐지면 합치기
-//
-//                marker.width = Marker.SIZE_AUTO // 정수 입력해서 변환 가능
-//                marker.height = Marker.SIZE_AUTO
-////              marker.anchor = PointF(100f, 1f) // 마커를 해당지점보다 왼쪽으로, 위쪽으로 떨어지게 만들기
-//
-//
-//                // marker1에 대한 정보 저장
+//                // marker에 대한 정보 저장
 //                val marker1Info = "위도: ${marker.position.latitude}, 경도: ${marker.position.longitude}"
 //                marker.tag = marker1Info // 마커 태그에 정보 입력
 //
@@ -210,57 +294,7 @@ class MainActivity : FragmentActivity(), OnMapReadyCallback {
 
 
 
-        //클라이언트 객체 생성
-        val naverMapApiInterface = NaverMapRequest.getClient().create(NaverMapApiInterface::class.java)
 
-        //응답 받을 콜백 구현
-//            val call = naverMapApiInterface.getNaverMapData()
-        val call: Call<NaverMapItem> = naverMapApiInterface.getMapData()
-
-        Log.v("Debug중","디버그중입니다.")
-
-        //클라이언트 객체가 제공하는 enqueue로 통신에 대한 요청, 응답 처리 방법 명시
-        call.enqueue(object : Callback<NaverMapItem> {
-            override fun onResponse(call: Call<NaverMapItem>, response: Response<NaverMapItem>) {
-                if(response.isSuccessful){
-                    Log.v("디버깅중", "성공!!!!!")
-                    naverMapList = response.body()
-                    naverMapInfo = naverMapList?.jjwifi
-
-
-//                        Toast.makeText(this@MainActivity, naverMapInfo?.get(1)?.address, Toast.LENGTH_LONG).show()
-
-                    naverMapInfo?.let{
-                        for(i in 0 until it.size){
-//                                val markers = arrayOfNulls<Marker>(it.size)
-
-//                                val marker = markers[i]
-                            val lat = it.get(i).y
-                            val lnt = it.get(i).x
-
-                            s_marker.position = LatLng(lat, lnt)
-                            //클러스터링
-                            clusterer.add(ItemKey(i, LatLng(it.get(i).y, it.get(i).x)), null)
-
-                        }
-                    }
-//                        marker.map = naverMap
-                    clusterer.map = naverMap
-
-                }
-
-            }
-            override fun onFailure(call: Call<NaverMapItem>, t: Throwable) {
-                // 통신 실패 시 처리할 코드
-                Log.v("디버깅중", "실패!!!!!")
-            }
-        })
-        ///////////////////////선빈 부분 ///////////////////////////////////
-
-
-        naverMap.locationSource = locationSource // 기존에 설정된 위치 소스 초기화
-        naverMap.locationTrackingMode = LocationTrackingMode.Follow // 위치 추적 모드를 Follow로 설정
-        naverMap.uiSettings.isLocationButtonEnabled = true // 현위치 버튼
 
         //실내지도 활성화
 //        naverMap.isIndoorEnabled = true
@@ -287,8 +321,4 @@ class MainActivity : FragmentActivity(), OnMapReadyCallback {
 
 
     }
-    //    companion object {
-//        private const val LOCATION_PERMISSION_REQUEST_CODE = 100
-//    }
-
 }
