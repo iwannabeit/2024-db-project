6e2a438e67aca26d95729949e6bd4ef49243443e
iwannabeit <140404545+iwannabeit@users.noreply.github.com>
2024-05-28 03:34:52 +0900

    inside-outside success

--- kotlin-java-section/app/src/main/java/com/example/db_wifi/MainActivity.kt
+++ kotlin-java-section/app/src/main/java/com/example/db_wifi/MainActivity.kt
@@ -1,27 +1,26 @@
 package com.example.db_wifi
 
+import android.Manifest
 import android.annotation.SuppressLint
+import android.content.ClipData.Item
 import android.content.pm.PackageManager
 import android.graphics.Color
-import android.health.connect.datatypes.units.Length
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.annotation.UiThread
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
-import androidx.core.location.LocationManagerCompat.requestLocationUpdates
 import androidx.core.view.GravityCompat
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.FragmentActivity
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.CameraUpdate
 import com.naver.maps.map.LocationTrackingMode
 import com.naver.maps.map.MapFragment
 import com.naver.maps.map.NaverMap
@@ -30,20 +29,20 @@ import com.naver.maps.map.OnMapReadyCallback
 import com.naver.maps.map.clustering.Clusterer
 import com.naver.maps.map.overlay.Marker
 import com.naver.maps.map.util.FusedLocationSource
+import com.naver.maps.map.util.MarkerIcons
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import com.naver.maps.map.util.MarkerIcons
-import android.Manifest
 
 
 //import com.naver.maps.map.CameraUpdate
 class MainActivity : FragmentActivity(), OnMapReadyCallback {
 
+    private var naverMapApiInterface = NaverMapRequest.getClient().create(NaverMapApiInterface::class.java)
     private var naverMapInfo: List<NaverMapData>? = null
     private var naverMapList: NaverMapItem? = null
-  
-    private var clusterer: Clusterer<ItemKey> = Clusterer.Builder<ItemKey>().screenDistance(50.0).build()
+
+//    private var clusterer: Clusterer<ItemKey> = Clusterer.Builder<ItemKey>().screenDistance(50.0).build()
 
     // FusedLocationProviderClient는 manifest에서 위치권한 얻은 후 사용할 수 있습니다!
     private lateinit var fusedLocationClient: FusedLocationProviderClient
@@ -55,6 +54,11 @@ class MainActivity : FragmentActivity(), OnMapReadyCallback {
     private lateinit var naverMap: NaverMap
     private lateinit var drawerLayout: DrawerLayout
     private lateinit var markerInfoText: TextView
+
+    //실내 실외 버튼 코드
+    private lateinit var indoorBtn : Button
+    private lateinit var outdoorBtn : Button
+
 
     // 위치 권한 요청
     private val requestPermissionLauncher =
@@ -73,6 +77,9 @@ class MainActivity : FragmentActivity(), OnMapReadyCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        indoorBtn = findViewById(R.id.indoor_button)
+        outdoorBtn = findViewById(R.id.outdoor_button)
 
         // FusedLocationProviderClient 초기화
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
@@ -141,58 +148,85 @@ class MainActivity : FragmentActivity(), OnMapReadyCallback {
             }
     }    // 위치 정보 요청
 
-
     private fun openDrawerWithMarkerInfo(markerInfo: String) {
         markerInfoText.text = markerInfo // 슬라이딩 드로어에 마커 정보 설정
         drawerLayout.openDrawer(GravityCompat.START) // 슬라이딩 드로어 열기
     }
+
         override fun onMapReady(naverMap: NaverMap) {
             this.naverMap = naverMap // naverMap 변수 초기화
 
             ///////////////////선빈 작업 구간////////////////////////////////////////////////////////
             //전주 시청으로 카메라 시점 시작
-//            val cameraUpdate = CameraUpdate.scrollTo(LatLng(35.8247083, 127.147528))
-//            naverMap.moveCamera(cameraUpdate)
+            val cameraUpdate = CameraUpdate.scrollTo(LatLng(35.8247083, 127.147528))
+            naverMap.moveCamera(cameraUpdate)
+
 
             // 마커 띄우는 곳!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-            val s_marker = Marker()
+//            val s_marker = Marker()
 
             //클라이언트 객체 생성
             val naverMapApiInterface = NaverMapRequest.getClient().create(NaverMapApiInterface::class.java)
 
             //응답 받을 콜백 구현
-//            val call = naverMapApiInterface.getNaverMapData()
             val call: Call<NaverMapItem> = naverMapApiInterface.getMapData()
-
-            Log.v("Debug중","디버그중입니다.")
 
             //클라이언트 객체가 제공하는 enqueue로 통신에 대한 요청, 응답 처리 방법 명시
             call.enqueue(object : Callback<NaverMapItem> {
                 override fun onResponse(call: Call<NaverMapItem>, response: Response<NaverMapItem>) {
-                    if(response.isSuccessful){
+                    if(response.isSuccessful) {
                         Log.v("디버깅중", "성공!!!!!")
                         naverMapList = response.body()
                         naverMapInfo = naverMapList?.jjwifi
 
+                        val indoorMarkers = mutableListOf<Marker>()
+                        val outdoorMarkers = mutableListOf<Marker>()
 
-//                        Toast.makeText(this@MainActivity, naverMapInfo?.get(1)?.address, Toast.LENGTH_LONG).show()
+                        val in_clusterer: Clusterer<ItemKey> = Clusterer.Builder<ItemKey>().screenDistance(50.0).build()
+                        val out_clusterer: Clusterer<ItemKey> = Clusterer.Builder<ItemKey>().screenDistance(50.0).build()
 
-                        naverMapInfo?.let{
-                            for(i in 0 until it.size){
-//                                val markers = arrayOfNulls<Marker>(it.size)
 
-//                                val marker = markers[i]
-                                val lat = it.get(i).y
-                                val lnt = it.get(i).x
+                            naverMapInfo?.let {
+                                for (i in 0 until it.size) {
 
-                                s_marker.position = LatLng(lat, lnt)
-                                //클러스터링
-                                clusterer.add(ItemKey(i, LatLng(it.get(i).y, it.get(i).x)), null)
+                                    val marker = Marker()
+                                    marker.position = LatLng(it[i].y, it[i].x)
+//
+                                    if(it[i].side == "inside"){
+                                        indoorMarkers.add(marker)
+                                    }
+                                    else{
+                                        outdoorMarkers.add(marker)
+                                    }
 
+                                }
                             }
+                        indoorBtn.setOnClickListener {
+                            // 모든 야외 마커 숨기기
+                            outdoorMarkers.forEach { it.map = null }
+                            // 모든 실내 마커 표시하기
+                            indoorMarkers.forEach {
+//                                it.map = naverMap
+                                in_clusterer.add(ItemKey(it.hashCode(), it.position), null)
+                            }
+                            out_clusterer.map = null
+                            in_clusterer.map = naverMap
                         }
-//                        marker.map = naverMap
-                        clusterer.map = naverMap
+
+                        outdoorBtn.setOnClickListener {
+                            // 모든 실내 마커 숨기기
+                            indoorMarkers.forEach { it.map = null }
+                            // 모든 야외 마커 표시하기
+                            outdoorMarkers.forEach {
+//                                it.map = naverMap
+                                out_clusterer.add(ItemKey(it.hashCode(), it.position), null)
+                            }
+
+                            out_clusterer.map = naverMap
+                            in_clusterer.map = null
+                        }
+//                            s_marker.map = naverMap
+//                        clusterer.map = naverMap
 
                     }
 
@@ -202,6 +236,9 @@ class MainActivity : FragmentActivity(), OnMapReadyCallback {
                     Log.v("디버깅중", "실패!!!!!")
                 }
             })
+
+            Log.v("Debug중","디버그중입니다.")
+
             ///////////////////////선빈 부분 ///////////////////////////////////
 
 
@@ -209,33 +246,11 @@ class MainActivity : FragmentActivity(), OnMapReadyCallback {
             naverMap.locationTrackingMode = LocationTrackingMode.Follow // 위치 추적 모드를 Follow로 설정
             naverMap.uiSettings.isLocationButtonEnabled = true // 현위치 버튼
 
-            //실내지도 활성화
-//        naverMap.isIndoorEnabled = true
-//        naverMap.uiSettings.isIndoorLevelPickerEnabled = true // 실내지도 층 버튼
 
-            // 마커를 클릭했을 때의 동작 설정
-            val marker = Marker()
-            marker.position = LatLng(37.5670135, 126.9783740)
-            marker.map = naverMap
-            marker.icon = MarkerIcons.BLACK
-            marker.iconTintColor = Color.RED
-            marker.width = Marker.SIZE_AUTO
-            marker.height = Marker.SIZE_AUTO
-            marker.setOnClickListener {
-                val cameraPosition = naverMap.cameraPosition
 
-                val latitudeString = cameraPosition.target.latitude.toString() // 위도를 문자열로 변환
-                val longitudeString = cameraPosition.target.longitude.toString() // 경도를 문자열로 변환
-                val positionString : String = "$latitudeString $longitudeString" // 위도와 경도를 합쳐서 위치를 나타내는 문자열 생성
-
-                openDrawerWithMarkerInfo(positionString) // 마커에 대한 정보를 슬라이딩 드로어에 표시
-                true
-            }
 
 
         }
-    //    companion object {
-//        private const val LOCATION_PERMISSION_REQUEST_CODE = 100
-//    }
+
 
 }
